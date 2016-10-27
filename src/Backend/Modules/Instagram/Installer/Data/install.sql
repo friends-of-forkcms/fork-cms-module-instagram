47fe03bc2c6e249221cb36de381b413a8d55ad1a
Jeroen Desloovere <jeroendesloovere@gmail.com>
2016-10-27 10:45:07 +0200

    'user_id' must be varchar, since this gives problems with an integer

--- src/Backend/Modules/Instagram/Installer/Data/install.sql
+++ src/Backend/Modules/Instagram/Installer/Data/install.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `instagram_users` (
+CREATE TABLE IF NOT EXISTS `instagram_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) DEFAULT '',
-  `user_id` int(11) DEFAULT NULL,
-  `extra_id` int(11) DEFAULT NULL,
+  `username` varchar(30) NOT NULL DEFAULT '',
+  `user_id` varchar(50) NOT NULL DEFAULT '',
+  `extra_id` int(11) NOT NULL,
   `locked` enum('N','Y') NOT NULL DEFAULT 'N',
   `hidden` enum('N','Y') NOT NULL DEFAULT 'N',
   `created_on` datetime DEFAULT NULL,
